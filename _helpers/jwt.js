bf64775cbf6640c34cb2c173f8fd72daff01bf93
Ian Walston <ianwalston@Ians-Mac-mini.local>
2020-05-22 15:41:01 -0400

    import paths

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -1,5 +1,5 @@
 const expressJwt = require('express-jwt');
-const config = require('config.json');
+const config = require('../config.json');
 
 module.exports = jwt;
 
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
+const jwt = require('./_helpers/jwt');
+const errorHandler = require('./_helpers/error-handler');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
