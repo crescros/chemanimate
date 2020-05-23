b254eaa95e9672f1a1ab3092d79e329018a47393
Ian Walston <ianwalston@Ians-Mac-mini.local>
2020-05-23 08:50:46 -0400

    whitelist

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -6,23 +6,26 @@ module.exports = jwt;
 function jwt() {
     const { secret } = config;
     return expressJwt({ secret }).unless({
-        path: [
-            // public routes that don't require authentication
-            '/chemanimate-app1',
-            '/chemanimate-app1/',
-            '/chemanimate-app1/login',
-            '/chemanimate-app1/login/',
-            '/chemanimate-app1/login/style.css',
-            '/chemanimate-app1/app',
-            '/chemanimate-app1/app/',
-            '/chemanimate-app1/app/style.css',
-            '/chemanimate-app1/forgotpassword',
-            '/chemanimate-app1/forgotpassword/',
-            '/chemanimate-app1/forgotpassword/style.css',
-            '/chemanimate-app1/signup',
-            '/chemanimate-app1/signup/',
-            '/chemanimate-app1/signup/style.css',
-            '/chemanimate-app1/api/users/authenticate',
+        // path: [
+        //     // public routes that don't require authentication
+        //     '/chemanimate-app1',
+        //     '/chemanimate-app1/',
+        //     '/chemanimate-app1/login',
+        //     '/chemanimate-app1/login/',
+        //     '/chemanimate-app1/login/style.css',
+        //     '/chemanimate-app1/app',
+        //     '/chemanimate-app1/app/',
+        //     '/chemanimate-app1/app/style.css',
+        //     '/chemanimate-app1/forgotpassword',
+        //     '/chemanimate-app1/forgotpassword/',
+        //     '/chemanimate-app1/forgotpassword/style.css',
+        //     '/chemanimate-app1/signup',
+        //     '/chemanimate-app1/signup/',
+        //     '/chemanimate-app1/signup/style.css',
+        // ]
+        path:[
+            /chemanimate-app1\/app/,
+            /chemanimate-app1\/api\/users\/authenticate/
         ]
     });
 }
