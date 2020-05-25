62c81d051aeb52b583115f1db5c02e60a316535d
Ian Walston <ianwalston@Ians-Mac-mini.local>
2020-05-25 16:33:30 -0400

    redirect with no token

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -6,23 +6,6 @@ module.exports = jwt;
 function jwt() {
     const { secret } = config;
     return expressJwt({ secret }).unless({
-        // path: [
-        //     // public routes that don't require authentication
-        //     '/chemanimate-app1',
-        //     '/chemanimate-app1/',
-        //     '/chemanimate-app1/login',
-        //     '/chemanimate-app1/login/',
-        //     '/chemanimate-app1/login/style.css',
-        //     '/chemanimate-app1/app',
-        //     '/chemanimate-app1/app/',
-        //     '/chemanimate-app1/app/style.css',
-        //     '/chemanimate-app1/forgotpassword',
-        //     '/chemanimate-app1/forgotpassword/',
-        //     '/chemanimate-app1/forgotpassword/style.css',
-        //     '/chemanimate-app1/signup',
-        //     '/chemanimate-app1/signup/',
-        //     '/chemanimate-app1/signup/style.css',
-        // ]
         path:[
             /favicon.ico/, // for dev
             /chemanimate-app1\/app/,
