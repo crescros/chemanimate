28466ef45de5ae661a81353901ed440967a25c29
Ian Walston <ikwalston@comcast.net>
2020-05-24 21:50:53 -0400

    change password

--- users/user.service.js
+++ users/user.service.js
@@ -1,32 +1,50 @@
-﻿const config = require('../config.json');
-const jwt = require('jsonwebtoken');
-const fs = require('fs')
+﻿const config = require("../config.json");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 // users hardcoded for simplicity, store in a db for production applications
 
-
 module.exports = {
     authenticate,
-    getAll
+    getAll,
+    changePassword
 };
 
-const users = JSON.parse(fs.readFileSync('data/users.json', 'utf8'))
+const users = JSON.parse(fs.readFileSync("data/users.json", "utf8"));
 
 async function authenticate({ username, password }) {
-    const user = users.find(u => u.username === username.toLowerCase() && u.password === password);
+    const user = users.find(
+        (u) => u.username === username.toLowerCase() && u.password === password
+    );
     if (user) {
         const token = jwt.sign({ sub: user.id }, config.secret);
         const { password, ...userWithoutPassword } = user;
         return {
             ...userWithoutPassword,
-            token
+            token,
         };
     }
 }
 
 async function getAll() {
-    return users.map(u => {
+    return users.map((u) => {
         const { password, ...userWithoutPassword } = u;
         return userWithoutPassword;
     });
+}
+
+async function changePassword({ username, password, newpassword }) {
+    const userIndex = users.findIndex((u) => u.username === username.toLowerCase() && u.password === password
+    );
+
+    if (userIndex !== -1 ) {
+
+        console.log(userIndex)
+        let newUsers = [...users];
+        newUsers[userIndex] = { ...newUsers[userIndex], password: newpassword };
+        fs.writeFileSync("data/users.json", JSON.stringify(newUsers));
+
+        return "Password Changed"
+    }
+
 }
--- users/users.controller.js
+++ users/users.controller.js
@@ -4,6 +4,7 @@ const userService = require('./user.service');
 
 // routes
 router.post('/authenticate', authenticate);
+router.post('/changepassword', changePassword);
 router.get('/', getAll);
 
 module.exports = router;
@@ -17,5 +18,11 @@ function authenticate(req, res, next) {
 function getAll(req, res, next) {
     userService.getAll()
         .then(users => res.json(users))
+        .catch(err => next(err));
+}
+
+function changePassword(req, res, next) {
+    userService.changePassword(req.body)
+        .then(msg => msg ? res.json({ message: msg }) : res.status(400).json({ message: 'Username or password is incorrect' }) )
         .catch(err => next(err));
 }
