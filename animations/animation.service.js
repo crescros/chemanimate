347078c5a29a50c40e4c24d627e8628aa67bb6cc
Ian Walston <ianwalston@Ians-Mac-mini.local>
2020-06-04 18:19:18 -0400

    other animations

--- animations/animation.service.js
+++ animations/animation.service.js
@@ -11,7 +11,12 @@ const animations = JSON.parse(fs.readFileSync('data/animations.json', 'utf8'))
 
 async function getAll(params) {
     console.log(params.type)
-    if (params.type){
+
+    if (params.type === 'other'){
+        return animations.data.filter(a => {
+            return !["Single Replacement", "Double Replacement", "Synthesis", "Decomposition"].includes(a.series)
+        })
+    } else if (params.type){
         return animations.data.filter(a => {
             console.log(a)
             return a.series === params.type
